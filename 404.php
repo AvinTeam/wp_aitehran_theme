3dd53ffceae50fca83b1e6b8214dac0e133fbba5
Mohammadreza <rashidpour1995@gmail.com>
2025-12-14 14:39:40 +0330

    feat(TAI-11): add 404

--- 404.php
+++ 404.php
@@ -1,6 +1,23 @@
-<?php get_header(); 
-
-components('404');
+<?php get_header(); ?>
 
 
+<div class="container">
+
+    <div class="bg-secondary rounded-70 d-flex flex-column-reverse flex-lg-row justify-content-center align-items-center m-40 p-40 row-gap-3">
+
+        <div class="col-12 col-lg-6 d-flex flex-column justify-content-center align-items-center">
+            <img class="w-280" src="<?php echo get_the_image_url( '404.png' ) ?>" alt="404">
+        </div>
+
+        <div class="col-12 col-lg-6 d-flex flex-column justify-content-center align-items-center row-gap-3">
+
+            <h2 class="text-white">یه مشکلی تووکاره !!!</h2>
+            <h4 class="text-white">باید صفحه دیگه ای رو بررسی کنید</h4>
+            <button class="btn btn-warning btn-lg">ورود به صفحه نخست</button>
+
+        </div>
+    </div>
+</div>
+
+<?php
 get_footer(); ?>
