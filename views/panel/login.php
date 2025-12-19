4d12998b6d60d1e4be6c8693d69aca66719dd380
Mohammadreza <77006263+mrrashidpour@users.noreply.github.com>
2025-12-19 10:32:44 +0330

    feat(TAI-32): add form Verification OTP

--- views/panel/login.php
+++ views/panel/login.php
@@ -22,29 +22,51 @@
                 <section class="d-flex flex-column row-gap-3 w-100 bg-gray rounded-65 py-40 px-100 ">
 
                     <form id="loginForm">
-                        <div id="mobileForm" class="d-flex flex-column row-gap-2">
-                            <div class="d-flex flex-column row-gap-2 " >
-                                <label class="f-24 fw-bold" for="mobile">شماره تماس</label>
-                                <input type="text" inputmode="numeric" pattern="\d*"
-                                class="d-ltr px-2 onlyNumbersInput w-100 rounded-32 overflow-hidden border-1 border border-black f-24 fw-bold"
-                                    id="mobile" maxlength="11" aria-describedby="sendsms">
+                        <div id="mobileForm">
+
+                            <div class="d-flex flex-column row-gap-2">
+                                <div class="d-flex flex-column row-gap-2 ">
+                                    <label class="f-24 fw-bold" for="mobile">شماره تماس</label>
+                                    <input type="text" inputmode="numeric" pattern="\d*"
+                                        class="text-center p-2 onlyNumbersInput w-100 rounded-32 overflow-hidden border-1 border border-black f-24 fw-bold"
+                                        id="mobile" maxlength="11" aria-describedby="sendsms">
+                                </div>
+
+                                <div class="d-flex flex-column row-gap-2 ">
+                                    <label class="f-24 fw-bold" for="captcha">کد امنیتی</label>
+                                    <input type="text" id="captcha" maxlength="5"
+                                        class="w-100 rounded-32 overflow-hidden border-1 border border-black f-24 fw-bold text-center p-2">
+                                </div>
+                                <button id="send-code" class="btn btn-warning fw-bold f-20 mt-5 mb-3 rounded-40"
+                                    type="submit" disabled>ارسال کد امنیتی به شماره همراه</button>
+
                             </div>
 
-                            <div class="d-flex flex-column row-gap-2 ">
-                                <label class="f-24 fw-bold" for="captcha">کد امنیتی</label>
-                                <input type="text" id="captcha" maxlength="5"
-                                class="w-100 rounded-32 overflow-hidden border-1 border border-black f-24 fw-bold d-ltr px-2" >
-                            </div>
-                            <button id="send-code" class="btn btn-warning fw-bold f-20 mt-5 mb-3 rounded-40" type="submit" disabled>ارسال کد امنیتی به شماره همراه</button>
                         </div>
                         <div id="codeVerification" style="display: none;">
-                            <input autocomplete="one-time-code" type="text" inputmode="numeric" pattern="\d*"
-                                class="onlyNumbersInput" id="verificationCode" placeholder="کد تایید را وارد کنید"
-                                aria-describedby="verify">
-                            <div id="timer">00:00</div>
-                            <button type="submit" id="verifyCode" disabled>تایید کد</button>
-                            <button type="button" id="resendCode" disabled>ارسال مجدد کد</button>
-                            <button type="button" id="editNumber">ویرایش شماره</button>
+                            <div class="d-flex flex-column row-gap-2">
+
+
+                                <div class="d-flex flex-column row-gap-2 ">
+                                    <label class="f-24 fw-bold" for="mobile">کد تایید</label>
+                                    <input autocomplete="one-time-code" type="text" inputmode="numeric" pattern="\d*"
+                                        class="text-center p-2 onlyNumbersInput w-100 rounded-32 overflow-hidden border-1 border border-black f-24 fw-bold"
+                                        id="verificationCode" aria-describedby="verify">
+                                </div>
+
+                                <div class="d-flex flex-row justify-content-around align-items-center">
+
+                                    <div id="timer">00:00</div>
+                                    <button type="button" id="resendCode" disabled>ارسال مجدد کد</button>
+                                </div>
+
+                                <button type="submit" id="verifyCode"
+                                    class="btn btn-warning fw-bold f-20 mt-5 mb-3 rounded-40" disabled>تایید کد</button>
+                                <button type="button" id="editNumber" class="btn btn-secondary rounded-40">ویرایش
+                                    شماره</button>
+
+                            </div>
+
                         </div>
                     </form>
 
